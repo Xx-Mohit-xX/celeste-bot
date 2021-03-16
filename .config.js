a78927a0c058eb11511549a45f75f74a6cc22eaf
Eamon <eamon.wong@outlook.com>
2021-03-16 13:23:06 +0800

    Create .config.js

---
+++ .config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  token: '',
+  prefix: ';',
+  mongodburi: 'mongodb://localhost:27017/databaseName',
+  database: 'databaseName',
+  collection: {
+    userdata: 'userdata',
+    islandinfo: 'islandinfo',
+    ban: 'ban',
+    mute: 'mute',
+    warn: 'warn',
+    modstats: 'modstats',
+    config: 'config',
+    reactrole: 'reactrole',
+  },
+  errorMessage: {
+    permission: 'You do not have permission to run this command.',
+    notMember: "User doesn't exist!",
+    noBotPermission: 'I don\'t have the permissions to run this command.',
+    noReason: 'Please indicate the reason',
+    noTime: 'Please indicate the time',
+  },
+};
