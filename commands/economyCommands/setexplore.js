791cbe4e904529b0d1b8946804eb45c88c498288
Eamon <eamon.wong@outlook.com>
2021-03-16 21:44:50 +0800

    base commit

---
+++ commands/economyCommands/setexplore.js
@@ -0,0 +1,24 @@
+module.exports = {
+  name: 'setexplore',
+  description: 'setexplore',
+  aliases: [],
+  usage: 'setexplore',
+  execute: async (client, message, config) => {
+    if (!message.member.roles.cache.some((r) => config.permissions.moderation.includes(r.id) || message.member.hasPermission(['ADMINISTRATOR']))) { message.reply('You\'re not allowed to use this command!'); return; }
+    const msgArr = message.content.split(' ');
+    if (!msgArr[1]) {
+      message.channel.send('configure explore amount first using !setexplore min-max');
+      return;
+    }
+    const rangeArr = msgArr[1].split('-');
+    const min = parseInt(rangeArr[0], 10);
+    const max = parseInt(rangeArr[1], 10);
+    if (isNaN(min) || isNaN(max)) {
+      message.channel.send('min and max must be integers!');
+      return;
+    }
+    message.channel.send('settings have been successfully applied!');
+    config.explore = { min, max };
+    await client.db.config.updateOne({ id: message.guild.id }, { $set: { explore: config.explore } }, { upsert: true });
+  },
+};
