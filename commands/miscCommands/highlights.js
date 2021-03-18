d2e3059f3b8fc725e3603e27249b72e1aa66fcb3
Eamon <eamon.wong@outlook.com>
2021-03-19 00:20:25 +0800

    working highlights

---
+++ commands/miscCommands/highlights.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable consistent-return */
+/* eslint-disable max-len */
+const Discord = require('discord.js');
+module.exports = {
+  name: 'highlights',
+  description: 'set guild configurations',
+  aliases: 'highlight',
+  usage: '!welcome',
+  execute: async (client, message, config) => {
+
+    const msgArr = message.content.split(' ');
+
+    const userdata = await client.db.userdata.findOne({id: message.member.id, guildID: message.guild.id });
+    if (msgArr.length === 1) {
+      if (!userdata.highlightList) return message.channel.send('You\'ve got no highlight words!')
+      if (userdata.highlightList.length > 0) {
+        return message.channel.send(`Your highlight words are: **${userdata.highlightList.join('**,** ')}**!`);
+      } else {
+        return message.channel.send('You\'ve got no highlight words!');
+      }
+    }
+
+    if (msgArr[1] === 'add') {
+        await client.db.userdata.updateOne({ id: message.member.id, guildID: message.guild.id }, {
+          $push: {
+            highlightList: msgArr.slice(2).join(' '),
+          },
+        }, { upsert: true });
+        const embed = new Discord.MessageEmbed()
+        .setColor('GREEN')
+        .setDescription(`You have successfully added **${msgArr.slice(2).join(' ')}** to your highlights!`);
+        message.channel.send({embed: embed});
+
+    } else if (msgArr[1] === 'remove') {
+      config.channels.welcomeChannel = '';
+      await client.db.userdata.updateOne({ id: message.member.id, guildID: message.guild.id }, {
+        $pull: {
+          highlightList: msgArr.slice(2).join(' ')
+        }
+      }, { upsert: true }).catch((error) => {
+        return message.channel.send('There was an error removing the highlight from the database.')
+      });
+      const doneEmbed = new Discord.MessageEmbed()
+      .setColor('GREEN')
+      .setDescription(`**${msgArr.slice(2).join(' ')}** has been removed from your highlights!`);
+      message.channel.send({embed: doneEmbed});
+    } else {
+      message.channel.send('You need to specify whether you are adding or removing a highlight!');
+    }
+  },
+};
--- events/message.js
+++ events/message.js
@@ -2,13 +2,36 @@
 const initializeGuild = require('../utils/initializeGuild');
 const gainExp = require('../utils/gainExp');
 const msToString = require('../utils/msToString');
-
+const Discord = require('discord.js');
 module.exports = (client, distube, message) => {
   if (!client.ready) return;
   if (message.author.bot) return;
   const msg = message.content.toLowerCase();
   const commandName = msg.split(' ')[0].substring(1);
+  const date = () => {
 
+      var date = new Date();
+
+      var hour = date.getHours();
+      hour = (hour < 10 ? "0" : "") + hour;
+
+      var min  = date.getMinutes();
+      min = (min < 10 ? "0" : "") + min;
+
+      var sec  = date.getSeconds();
+      sec = (sec < 10 ? "0" : "") + sec;
+
+      var year = date.getFullYear();
+
+      var month = date.getMonth() + 1;
+      month = (month < 10 ? "0" : "") + month;
+
+      var day  = date.getDate();
+      day = (day < 10 ? "0" : "") + day;
+
+      return hour + ":" + min + ":" + sec;
+
+  }
   if (!message.member) {
     return;
   }
@@ -20,22 +43,27 @@ module.exports = (client, distube, message) => {
     return;
   }
   async function checkHighlights() {
-    const Guild = client.guilds.cache.get(message.guild.id);
-    const Members = Guild.members.cache.map(member => member.id);
-    const highlights = await client.db.userdata.find({highlightList: message.content})
-    if(!highlights) return;
-    console.log(highlights.highlightList);
-    console.log(highlights);
-    const [ list ] = highlights.highlightList;
-  //  for (var i = 0; i < highlights.highlightLight.length; i++) {
-  //    if (message.content.includes(highlights.highlightList[i])) {
-      list.forEach(user => {
-      client.users.cache.get(user.id).send('test');
-   })
-//  }
-//  }
+    const highlights = await client.db.userdata.find({  }).toArray()
+    highlights.forEach((user) => {
+      if (user.highlightList) {
+        const triggeredWord = user.highlightList.find((word) => message.content.toLowerCase().includes(word.toLowerCase()));
+        if (triggeredWord) {
+          let discordUser = client.users.cache.get(user.id);
+          if (discordUser) {
+            if (message.member.id === user.id) return;
+            const embed = new Discord.MessageEmbed()
+            .setColor('#5b4194')
+            .setTitle(triggeredWord)
+            .setDescription(`**\[${date()}\] ${message.member.displayName}**: ${message.content}`)
+            .addField('Source', `[Jump to](${message.url})`)
+            .setTimestamp()
+            discordUser.send(`In **${message.guild.name}** ${message.channel}, your highlight "${triggeredWord}" was mentioned.`, embed);
+      }
+    }
+  }
+})
+
 }
-  checkHighlights();
   if (message.content.startsWith(config.prefix)) {
     const command = client.commands.get(commandName);
     if (!client.commands.has(commandName)) return;
@@ -63,5 +91,6 @@ module.exports = (client, distube, message) => {
     }
   } else {
     gainExp(client, message, config);
+    checkHighlights();
   }
 };
