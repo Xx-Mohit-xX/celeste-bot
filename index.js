592b623b0bda6795af95fb137d7f0ec986305a7e
Eamon <eamon.wong@outlook.com>
2021-07-06 23:46:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,11 +22,10 @@ client.on('ready', () => {
 });
 client.on('guildMemberAdd', newMember => { //when someone new joins a guild
     client.user.setActivity(`${client.users.cache.size} users | ;help`, { type: 'LISTENING' }); //Update the activity every time someone joins a guild
-    function kickNewMembers() {
     try {
     console.log(newMember.user.avatarURL);
     console.log(newMember.user.displayAvatarURL);
-    if ((Date.now() - newMember.user.createdAt < 1000*60*60*24*7) && newMember.user.displayAvatarURL.includes('0.png')) {
+    if ((Date.now() - newMember.user.createdAt < 1000*60*60*24*7) && newMember.user.displayAvatarURL === 'https://cdn.discordapp.com/embed/avatars/0.png') {
       const newMemberBan = new Discord.MessageEmbed()
       .setColor('RED')
       .setDescription('Your account has been kicked from Polaris as it is too new. Please come back when your account is more than 7 days old.')
@@ -37,8 +36,6 @@ client.on('guildMemberAdd', newMember => { //when someone new joins a guild
   } catch(err) {
     console.log(err.stack)
   }
-}
-setTimeout(kickNewMembers, 8000);
 });
 client.on('messageDelete', messageDelete => {
   try {
